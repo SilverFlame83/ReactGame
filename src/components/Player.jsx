e6e237bee06e42dec428dc44c1b1930c20c6ffaf
VelvetSyntax <imren_83@abv.bg>
2024-05-20 17:25:49 +0300

    setting useRef, instead of useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  //const [enteredPlayer, setEnteredPlayer] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const userInput = useRef()
+
+  // function inputHandler(event) {
+  //   setSubmitted(false)
+  //   setEnteredPlayer(event.target.value);
+  // }
+
+  function clickHandler() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? userInput.current.value : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={userInput} />
+        <button onClick={clickHandler}>Set Name</button>
       </p>
     </section>
   );
